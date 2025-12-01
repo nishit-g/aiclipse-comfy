2861e12c39964ebd4f397ef00bf64f361f7e0455
Nishit Gupta <nishit.gupta6708@gmail.com>
2025-12-02 03:11:20 +0530

    just same env variables

--- base/scripts/start.sh
+++ base/scripts/start.sh
@@ -116,6 +116,12 @@ setup_environment() {
         "TEMPLATE_VERSION"
         "GPU_TYPE"
         "COMFY_ARGS"
+        "R2_ACCESS_KEY_ID"
+        "R2_SECRET_ACCESS_KEY"
+        "R2_ACCOUNT_ID"
+        "R2_BUCKET"
+        "HF_TOKEN"
+        "CIVITAI_TOKEN"
     )
 
     # Create environment file for SSH sessions
